a765205e1289f5bc8a8909cc20d1f758a42503cf
Giovanni Zambotti <g.zambotti@gmail.com>
2014-07-29 17:20:02 -0400

    update index.html and darmc.js

--- js/darmc.js
+++ js/darmc.js
@@ -1,20 +1,18 @@
 var map, toc, darmcLayer, identifyTask,identifyParams,identifyListener;
 			
             
-require(["dojo/_base/connect","dojo/dom", "dojo/parser", "dijit/registry", "dojo/on","esri/map","esri/toolbars/navigation","esri/layers/ArcGISTiledMapServiceLayer", 
-	"esri/layers/ArcGISDynamicMapServiceLayer","agsjs/dijit/TOC","dijit/layout/BorderContainer","dijit/layout/ContentPane",
-	"dijit/layout/AccordionContainer","dijit/layout/TabContainer","esri/dijit/Popup", "dijit/Toolbar","dojo/fx", "dojo/domReady!"], 
+require(["dojo/parser","dojo/_base/connect","dojo/dom", "dijit/registry", "dojo/on","esri/map","esri/toolbars/navigation","esri/layers/ArcGISTiledMapServiceLayer", 
+	"esri/layers/ArcGISDynamicMapServiceLayer","agsjs/dijit/TOC","esri/dijit/BasemapGallery","esri/dijit/Basemap", "esri/dijit/BasemapLayer","dijit/layout/BorderContainer","dijit/layout/ContentPane",
+	"dijit/layout/AccordionContainer","dijit/layout/TabContainer","esri/dijit/Popup", "dijit/Toolbar","dijit/form/Button", "dijit/Menu", "dojo/domReady!"], 
 
-	function(connect, dom, parser, registry, on, Map, Navigation, ArcGISTiledMapServiceLayer, ArcGISDynamicMapServiceLayer,TOC,BorderContainer,
-		ContentPane, AccordionContainer,TabContainer,Popup){
+	function(parser, connect, dom, registry, on, Map, Navigation, ArcGISTiledMapServiceLayer, ArcGISDynamicMapServiceLayer,TOC, BasemapGallery, Basemap, BasemapLayer, BorderContainer,
+		ContentPane, AccordionContainer,TabContainer,Popup, Toolbar){
             
 	// call the parser to create the dijit layout dijits
 	parser.parse(); // note djConfig.parseOnLoad = false;
+
 	var navToolbar;
-				
-	var popup = new esri.dijit.Popup({
-		fillSymbol: new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255,0,0]), 2), new dojo.Color([255,255,0,0.25]))
-	}, dojo.create("div"));
+	var popup = new Popup({},dojo.create("div"))
 				
 	map = new Map("map", {	
 		center: [12, 46],
@@ -24,25 +22,105 @@ require(["dojo/_base/connect","dojo/dom", "dojo/parser", "dijit/registry", "dojo
 	});
 
 	navToolbar = new Navigation(map);
-    //on(navToolbar, "onExtentHistoryChange", extentHistoryChangeHandler);
-
+    
     registry.byId("zoomin").on("click", function () {navToolbar.activate(Navigation.ZOOM_IN);});
     registry.byId("zoomout").on("click", function () {navToolbar.activate(Navigation.ZOOM_OUT);});
     registry.byId("zoomfullext").on("click", function () {navToolbar.zoomToFullExtent();});
     registry.byId("zoomprev").on("click", function () {navToolbar.zoomToPrevExtent();});
     registry.byId("zoomnext").on("click", function () {navToolbar.zoomToNextExtent();});
     registry.byId("pan").on("click", function () {navToolbar.activate(Navigation.PAN);});
+          
+	darmcLayer = new ArcGISDynamicMapServiceLayer("http://cga6.cga.harvard.edu/arcgis/rest/services/darmc/roman/MapServer");
+    //basemap1 = new ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/arcgis/rest/services/World_Shaded_Relief/MapServer");
+    //map.addLayer(basemap1);
+
+    var locator = new esri.tasks.Locator("http://cga6.cga.harvard.edu/arcgis/rest/services/darmc/DARMC_locator/GeocodeServer");
+	dojo.connect(locator, "onAddressToLocationsComplete", showResults);	
+
+    
+    var basemaps = [];                
+	var basemapOSM = new Basemap({layers: [new BasemapLayer({type: "OpenStreetMap"})], id: "bmOSM",	title: "Open Street Map"});
+	var basemapOcean = new Basemap({layers: [new BasemapLayer({type: "Ocean"})],id: "bmESRIOcean",title: "Ocean Basemap"});
+	var basemapTopo = new Basemap({layers: [new BasemapLayer({type: "Topo"})], id: "bmESRITopo", title: "Esri Topo"});
+
+	basemaps.push(basemapOSM, basemapOcean, basemapTopo);
+	
+
+	var basemapGallery = new BasemapGallery({showArcGISBasemaps: true, basemaps: basemaps,	map: map}, "basemapGallery");
+												
+	basemapGallery.startup();
+	
+	dojo.forEach(basemapGallery.basemaps, function(basemap) {            
+		//Add a menu item for each basemap, when the menu items are selected
+		dijit.byId("basemapMenu").addChild(new dijit.MenuItem({
+			label: basemap.title,
+			onClick: dojo.hitch(this, function() {							
+				basemapGallery.select(basemap.id);
+			})
+		}));          
+	});			
+
+    basemapGallery.select('bmOSM');
+    registry.byId("locateButton").on("click", locate);	
+	function locate() {
+		map.graphics.clear();
+		var add = dojo.byId("placeName").value; //.split(",");
+		var address = {
+			Name : add,
+			County: ""
+		};
+		locator.addressToLocations(address,["Loc_name"]);
+	}
+			
+			function showResults(candidates) {
+				var candidate;
+				var symbol =  new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 8, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([0,43,255]), 1), new dojo.Color([162,245,239,0.5]));
+				var infoTemplate = new esri.InfoTemplate("Location", "Name: ${address}<br />Score: ${score}<br />");
+				candidate_holder = []
+				var geom;
+				var uExtent=null;
+				var iBuffer=3000;
+				var sResult="";
+				dojo.every(candidates,function(candidate){					
+					//if (candidate.score > 0) {
+						console.log(candidate.score);
+						var attributes = { address: candidate.address, score:candidate.score};
+						geom = esri.geometry.geographicToWebMercator(candidate.location);
+						/*if (uExtent==null) {
+							uExtent=new esri.geometry.Extent(geom.x-iBuffer,geom.y-iBuffer,geom.x+iBuffer, geom.y+iBuffer);
+						} 
+						else {
+							uExtent=uExtent.union(new esri.geometry.Extent(geom.x-iBuffer,geom.y-iBuffer,geom.x+iBuffer, geom.y+iBuffer))
+						}*/
+						var graphic = new esri.Graphic(geom, symbol, attributes, infoTemplate);
+						//add a graphic to the map at the geocoded location
+						map.graphics.add(graphic);
+						//add a text symbol to the map listing the location of the matched address.
+						var displayText = candidate.address;
+						var font = new esri.symbol.Font("8pt",esri.symbol.Font.STYLE_NORMAL, esri.symbol.Font.VARIANT_NORMAL,esri.symbol.Font.WEIGHT_BOLD,"Helvetica");
+						var textSymbol = new esri.symbol.TextSymbol(displayText,font,new dojo.Color("#000"));
+						textSymbol.setOffset(0,8);
+						candidate_holder.push(candidate.score)
+						//sResult+="<a href='JavaScript:zoomToIt(" +geom.x + "," + geom.y + ")'>" + candidate.address + "</a><br />";
+						map.graphics.add(new esri.Graphic(geom, textSymbol));
+						return true; //break out of loop after one candidate with score greater  than 80 is found.
+					//}
+					//else{console.log("text");alert("No place found! Please try it again.");}
+					
+				});
+				//map.setExtent(uExtent, true);
+				console.log(candidate_holder, candidate_holder.sort()[candidate_holder.length -1])
+				if(candidate_holder.sort()[candidate_holder.length -1] < 40 || candidate_holder.sort()[candidate_holder.length -1] == undefined){alert("No place found! Please try it again.");}
+			//sResult+="<br/><a href='JavaScript:dojo.byId(\"geocodeResult\").style.visibility=\"hidden\";return false'>Close</a>";
+			//dojo.byId("geocodeResult").innerHTML=sResult;
+			//dojo.byId("geocodeResult").style.visibility="visible";
+			
+			//map.centerAndZoom(geom,12);
+		}
+	
+    
 
 
-
-    /*function extentHistoryChangeHandler () {
-        registry.byId("zoomprev").disabled = navToolbar.isFirstExtent();
-        registry.byId("zoomnext").disabled = navToolbar.isLastExtent();
-    }*/
-              
-	darmcLayer = new ArcGISDynamicMapServiceLayer("http://cga6.cga.harvard.edu/arcgis/rest/services/darmc/roman/MapServer", {opacity: 0.8});
-    basemap = new ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/arcgis/rest/services/World_Shaded_Relief/MapServer");
-    map.addLayer(basemap);         
     map.on('layers-add-result', function(evt){
         // overwrite the default visibility of service. TOC will honor the overwritten value.
         darmcLayer.setVisibleLayers([54,58]);
@@ -67,10 +145,12 @@ require(["dojo/_base/connect","dojo/dom", "dojo/parser", "dijit/registry", "dojo
 				map.addLayers([darmcLayer]);
 				dojo.connect(map,"onLoad",mapReady);
             });
-			
+		
+	
+
 		function mapReady(map){				
 			//identifyTask = new esri.tasks.IdentifyTask("http://cga6.cga.harvard.edu/arcgis/rest/services/darmc/roman/MapServer");
-			identifyTask = new esri.tasks.IdentifyTask("http://cga1.cga.harvard.edu/arcgis/rest/services/darmc/roman/MapServer");
+			identifyTask = new esri.tasks.IdentifyTask("http://cga6.cga.harvard.edu/arcgis/rest/services/darmc/roman/MapServer");
 			identifyParams = new esri.tasks.IdentifyParameters();	
 			identifyListener = dojo.connect(map,"onClick",executeIdentifyTask);				
 			dojo.connect(map,"onClick",function (){
@@ -227,6 +307,12 @@ require(["dojo/_base/connect","dojo/dom", "dojo/parser", "dijit/registry", "dojo
 			});
 			map.infoWindow.setFeatures([ deferred ]);
 			map.infoWindow.show(evt.mapPoint);
-		}
 
-		//document.getElementsByClassName("agsjsTOCServiceLayerPD").onclick = function(){console.log("test")}
+			
+
+	}
+
+
+
+		
+			
